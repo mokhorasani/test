f6f3e1f79e665ae7d378979287d1c4806f7c3ebd
mokhorasani <53317758+mokhorasani@users.noreply.github.com>
2021-11-25 12:20:35 +0300

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,7 +1,4 @@
 import streamlit as st
 
-st.title('Hello world')
-st.write('From ***Streamlit Cloud***')
-
 st.write('**Secret:**',st.secrets['secret'])
 st.write('**Password:**',st.secrets['section']['password'])
